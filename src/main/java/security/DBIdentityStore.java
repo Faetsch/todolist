a32f5dcf4f03a19c9b93a03eedea0b568780f785
Fatih Kesikli <fatih.kesikli@enowa.ag>
2020-11-25 15:30:28 +0100

    - shitty authentification für login - keine authentification für api

--- src/main/java/security/DBIdentityStore.java
+++ src/main/java/security/DBIdentityStore.java
@@ -1,17 +1,9 @@
 package security;
 
-import entities.UserLoginDetails;
-import service.DatabaseService;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.security.enterprise.credential.Credential;
-import javax.security.enterprise.credential.UsernamePasswordCredential;
-import javax.security.enterprise.identitystore.CredentialValidationResult;
 import javax.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
 import javax.security.enterprise.identitystore.IdentityStore;
-import java.util.Arrays;
-import java.util.HashSet;
+
 
 @ApplicationScoped
 @DatabaseIdentityStoreDefinition(
@@ -20,21 +12,7 @@ import java.util.HashSet;
         useFor = IdentityStore.ValidationType.VALIDATE,
         hashAlgorithm = ShittyPasswordHash.class
 )
+
 public class DBIdentityStore implements IdentityStore
 {
-    @Inject
-    DatabaseService ds;
-
-    //wird nicht aufgerufen mit @DBIdentityStoreDefintion gesetzt
-//    @Override
-//    public CredentialValidationResult validate(Credential credential)
-//    {
-//        UsernamePasswordCredential login = (UsernamePasswordCredential) credential;
-//        UserLoginDetails details = ds.findLoginDetailsByUsername(login.getCaller());
-//        if(details != null && details.getPassword().equals(login.getPasswordAsString()))
-//        {
-//            return new CredentialValidationResult(login.getCaller(), new HashSet<>(Arrays.asList("ADMIN")));
-//        }
-//        else return CredentialValidationResult.NOT_VALIDATED_RESULT;
-//    }
 }
